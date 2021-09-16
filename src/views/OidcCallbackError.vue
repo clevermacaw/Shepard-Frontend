faa3ecdf3cdde2a0ec36b7bd0e190cdb373d0b19
Alex Lim <cleverbluemaccaw@gmail.com>
2021-09-16 08:39:48 +0200

    Add note about oidc error

--- src/views/OidcCallbackError.vue
+++ src/views/OidcCallbackError.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Error in authentication</h1>
+    This can be a false positive. Give it a try and visit the
+    <b-link :to="{ name: 'Explore' }">main page</b-link>.
   </div>
 </template>
 
