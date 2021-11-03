18e128aafa040fa070320749c841c0e21cf849ce
Alex Lim <cleverbluemaccaw@gmail.com>
2021-11-03 08:18:37 +0000

    Create URI Ref

---
+++ src/components/references/UriReferenceModal.vue
@@ -0,0 +1,87 @@
+<template>
+  <b-modal
+    :id="modalId"
+    ref="modal"
+    size="lg"
+    :title="modalName"
+    lazy
+    @ok="$emit('create', newURIReference)"
+  >
+    <b-form-group>
+      <b-container>
+        <b-row class="mb-3">
+          <b-col cols="2"> Name </b-col>
+          <b-col cols="8">
+            <b-form-input
+              id="name-input"
+              v-model="newURIReference.name"
+              required
+              placeholder="Name"
+            >
+            </b-form-input>
+          </b-col>
+        </b-row>
+
+        <b-row class="mb-3">
+          <b-col cols="2"> URI </b-col>
+          <b-col cols="8">
+            <b-form-input
+              id="uri-input"
+              v-model="newURIReference.uri"
+              required
+              placeholder="please insert URI"
+            >
+            </b-form-input>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-form-group>
+  </b-modal>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { URIReference } from "@dlr-shepard/shepard-client";
+
+declare interface URIReferenceModelData {
+  newURIReference: URIReference;
+}
+
+export default Vue.extend({
+  props: {
+    modalId: {
+      type: String,
+      default: "URIReferenceModal",
+    },
+    modalName: {
+      type: String,
+      default: "URIReferenceModal",
+    },
+    currentCollectionId: {
+      type: Number,
+      required: true,
+    },
+    currentDataObjectId: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      newURIReference: {
+        name: "",
+        uri: "",
+      },
+    } as URIReferenceModelData;
+  },
+});
+</script>
+
+<style scoped>
+.fixed-width {
+  width: 40px;
+  height: 40px;
+  padding: 0px;
+}
+</style>
--- src/components/references/UriReferencesList.vue
+++ src/components/references/UriReferencesList.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="list">
+  <div>
+    <b-button v-b-modal.create-uri-ref-modal class="mb-3" variant="primary">
+      Create new Reference
+    </b-button>
+
+    <UriReferenceModal
+      :current-collection-id="currentCollectionId"
+      :current-data-object-id="currentDataObjectId"
+      modal-id="create-uri-ref-modal"
+      modal-name="Create URI Reference"
+      @create="create($event)"
+    />
+
     <b-list-group>
       <b-list-group-item v-for="(uriItem, index) in uriList" :key="index">
         <div>
@@ -19,6 +31,7 @@
 import Vue, { VueConstructor } from "vue";
 import { URIReference } from "@dlr-shepard/shepard-client";
 import { UriReferenceVue } from "@/utils/api-mixin";
+import UriReferenceModal from "@/components/references/UriReferenceModal.vue";
 import CreatedByLine from "@/components/generic/CreatedByLine.vue";
 
 declare interface URIListData {
@@ -28,7 +41,7 @@ declare interface URIListData {
 export default (
   Vue as VueConstructor<Vue & InstanceType<typeof UriReferenceVue>>
 ).extend({
-  components: { CreatedByLine },
+  components: { CreatedByLine, UriReferenceModal },
   mixins: [UriReferenceVue],
   props: {
     currentCollectionId: {
@@ -60,6 +73,21 @@ export default (
         })
         .catch(e => {
           console.log("Error while fetching URI References " + e);
+        })
+        .finally();
+    },
+    create(newReference: URIReference) {
+      this.uriReferenceApi
+        ?.createUriReference({
+          collectionId: this.currentCollectionId,
+          dataObjectId: this.currentDataObjectId,
+          uRIReference: newReference,
+        })
+        .then(response => {
+          this.uriList = { ...this.uriList, ...[response] };
+        })
+        .catch(e => {
+          console.log("Error while creating URIReference " + e);
         })
         .finally();
     },
